5dc2baf24701a4a7ed2d8603c35a422936cae19f
sofi <est.minich.sofia@latecnicalf.com.ar>
2026-09-23 15:40:06 -0300

    faltaaaaaa

--- admin/votacion.php
+++ admin/votacion.php
@@ -1,5 +1,4 @@
 <?php
-
 echo"<!DOCTYPE html>
 <html lang='en'>
 <body>
@@ -7,4 +6,29 @@ echo"<!DOCTYPE html>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Inter</title>
-</head>";
+</head>
+<body>
+    <header>
+         <a href=''><img src='logo' alt=''></a>
+        <a href=''>inicio</a>
+        <a href='datos.html'>Inter</a>
+    </header>";
+    $votacion=false;/*arreglar despues*/
+if($votacion == false)
+{
+    echo"<body>
+            <div class= votacion;>
+                <a href='votacion.php'>Iniciar votacion</a>
+            </div>
+        </body>";
+        $votacion=true;
+}
+else
+    {
+        echo"<body>
+            <div class= votacion;>
+                <a href='votacion.php'>Cerrar votacion</a>
+            </div>
+        </body>";
+        $votacion=false;
+}
