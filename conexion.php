ac07155cfa7048cc991ee8060b1eda88c5e49443
Lucas <est.maisonnave.lucas@latecnicalf.com.ar>
2026-09-23 16:20:45 -0300

    muchos cambios

--- conexion.php
+++ conexion.php
@@ -7,12 +7,5 @@ $base_datos = "desafio intertribus";
 $conexion = new mysqli($servidor, $usuario, $password, $base_datos);
 
 
-if ($conexion->connect_error) {
-    die("Conexión a la base de datos fallida: " . $conexion->connect_error);
-}
-else{
-    echo "Conexion exitosa a la base de datos";
-}
 
-$conexion->close();
 ?>
