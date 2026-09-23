d9caffb9cb7a3d875ab1fb055b464da36f8663c1
sofi <est.minich.sofia@latecnicalf.com.ar>
2026-09-23 16:31:27 -0300

    aun noo

--- admin/buscar.php
+++ admin/buscar.php
@@ -1,3 +1,24 @@
 <?php
 session_start();
-$id = $_SESSION['id'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busqueda</title>
+</head>
+<body>
+    <form action="buscar/busqueda.php" method="POST">
+       
+        <p>Ingresa el ID del usuario</p>
+        <input
+          type="text"
+          class="input"
+          name="id"
+          required
+        />
+        <input type="submit" value="Buscar usuario" class="button" />
+      </form>
+</body>
+</html>
