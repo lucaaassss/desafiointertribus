8e29b92d1165ad574e20d9d7be196f23acbcb84d
fodi <est.minich.sofia@latecnicalf.com.ar>
2026-09-23 14:57:47 -0300

    usuarios

---
+++ admin/agregarusuarios.php
@@ -0,0 +1,30 @@
+<?php
+echo"<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Inter</title>
+</head>
+
+<body>
+
+    <head>
+        <a href=''><img src='logo' alt=''></a>
+        <a href='usuario.html'>Usuario</a>
+        <a href='datos.html'>Inter</a>
+    </head>
+
+    <div class='info'>
+    <div class='titulo'><h1>USUARIOS</h1></div>
+        <div class='boton'>
+        <a href=' '>agregar usuario</a>
+        <a href=' '>Eliminar usuario</a>
+        </div>
+    </div>
+
+</body>
+
+</html>";
+?>
